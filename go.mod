3f9686e92f6ed499544ec19f2a67578ec5fffe5f
aditya.kumar <aditya1944@live.com>
2025-12-30 00:19:51 +0530

    initial implementation of rate limiter

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aditya1944/rate-limiter
+
+go 1.25.5
---
+++ ratelimiter_test.go
@@ -0,0 +1,247 @@
+package ratelimiter
+
+import (
+	"fmt"
+	"math"
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
+func TestInput(t *testing.T) {
+	t.Parallel()
+
+	tcs := []struct {
+		name        string
+		tokenRate   float64
+		burstSize   uint
+		shouldPanic bool
+	}{
+		{
+			name:        "token rate is lower than burst size",
+			tokenRate:   21,
+			burstSize:   32,
+			shouldPanic: false,
+		},
+		{
+			name:        "when tokenrate and burstSize is MaxUint",
+			tokenRate:   math.MaxUint,
+			burstSize:   math.MaxUint,
+			shouldPanic: true,
+		},
+		{
+			name:        "boundary condition for code to not panic",
+			tokenRate:   math.MaxUint / 5000,
+			burstSize:   0,
+			shouldPanic: false,
+		},
+		{
+
+			name:        "boundary condition for code to panic",
+			tokenRate:   (math.MaxUint / 5000) + 1,
+			burstSize:   0,
+			shouldPanic: true,
+		},
+		{
+			name:        "token rate is higher than burst size",
+			tokenRate:   23,
+			burstSize:   20,
+			shouldPanic: false,
+		},
+		{
+			name:        "token rate is negative",
+			tokenRate:   -2.34,
+			burstSize:   23,
+			shouldPanic: true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			defer func() {
+				r := recover()
+				if tc.shouldPanic && r == nil {
+					t.Error("expected panic, but code did not panic")
+				}
+				if !tc.shouldPanic && r != nil {
+					t.Error("expected code to not panic, but code panicked")
+				}
+			}()
+			limiter := New(tc.tokenRate, tc.burstSize)
+			limiter.Close()
+		})
+	}
+}
+
+func TestAllow(t *testing.T) {
+	t.Parallel()
+
+	tcs := []struct {
+		name      string
+		tokenRate float64
+		burstSize uint
+		requests  int
+		allowed   int
+	}{
+		{
+			name:      "when tokenrate is 10 and burstsize is 10",
+			tokenRate: 10,
+			burstSize: 10,
+			requests:  10,
+			allowed:   10,
+		},
+		{
+			name:      "when token rate is greater than burstsize",
+			tokenRate: 11,
+			burstSize: 10,
+			requests:  11,
+			allowed:   10,
+		},
+		{
+			name:      "when burst size is 0",
+			tokenRate: 10,
+			burstSize: 0,
+			requests:  5,
+			allowed:   0,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			rateLimiter := New(tc.tokenRate, tc.burstSize)
+			defer rateLimiter.Close()
+
+			allowedRequests := 0
+			for range tc.requests {
+				if allowed := rateLimiter.Allow("key"); allowed {
+					allowedRequests += 1
+				}
+			}
+
+			if allowedRequests != tc.allowed {
+				t.Errorf("expected allowed requests: %d, got : %v", allowedRequests, tc.allowed)
+			}
+		})
+	}
+}
+
+func TestAllowWhenKeyIsEvictedFromCache(t *testing.T) {
+
+	synctest.Test(t, func(t *testing.T) {
+
+		rateLimiter := New(0.0167, 1) // one token per minute; burst size of 1
+		defer rateLimiter.Close()
+
+		if allowed := rateLimiter.Allow("user1"); !allowed {
+			t.Fatal("expected allowed to be true, got false")
+		}
+
+		if allowed := rateLimiter.Allow("user1"); allowed {
+			t.Fatal("expected allowed to be false, got true")
+		}
+
+		time.Sleep(1*time.Hour + 5*time.Minute)
+
+		synctest.Wait()
+
+		if allowed := rateLimiter.Allow("user1"); !allowed {
+			t.Fatal("expected key to be evicted, but it wasn't")
+		}
+	})
+}
+
+func TestAllowTokenRateFill(t *testing.T) {
+
+	synctest.Test(t, func(t *testing.T) {
+
+		rateLimiter := New(1, 10) // one token per second; burst size of 10
+		defer rateLimiter.Close()
+
+		for range 10 {
+
+			if allowed := rateLimiter.Allow("user1"); !allowed {
+				t.Fatal("expected allowed to be true, got false")
+			}
+		}
+
+		if allowed := rateLimiter.Allow("user1"); allowed {
+			t.Fatal("expected allowed to be false, got true")
+		}
+
+		time.Sleep(5 * time.Second)
+
+		synctest.Wait()
+
+		for range 5 {
+			if allowed := rateLimiter.Allow("user1"); !allowed {
+				t.Fatal("expected allowed to be true, got false")
+			}
+		}
+
+		if allowed := rateLimiter.Allow("user1"); allowed {
+			t.Fatal("expected allowed to be false, got true")
+		}
+	})
+}
+
+func TestAllowSteadyTraffic(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		rateLimiter := New(10, 10)
+		defer rateLimiter.Close()
+
+		for range 10 {
+			if !rateLimiter.Allow("key") {
+				t.Error("expected ratelimiter to allow, but not allowed")
+			}
+		}
+
+		if rateLimiter.Allow("key") {
+			t.Error("expected 11th request to not be allowed, but allowed")
+		}
+
+		time.Sleep(500 * time.Millisecond)
+		synctest.Wait()
+
+		for range 5 {
+			if !rateLimiter.Allow("key") {
+				t.Error("expected ratelimiter to allow, but not allowed")
+			}
+		}
+
+		if rateLimiter.Allow("key") {
+			t.Error("expected 6th request to be not allowed, but allowed")
+		}
+
+	})
+}
+
+func BenchmarkAllow(b *testing.B) {
+	rateLimiter := New(1000, 10000)
+	defer rateLimiter.Close()
+
+	i := 0
+
+	for b.Loop() {
+		rateLimiter.Allow(fmt.Sprintf("key%d", i))
+		i++
+		i = i % 10
+	}
+}
+
+func BenchmarkAllowParallel(b *testing.B) {
+	rateLimiter := New(1000, 10000)
+	defer rateLimiter.Close()
+
+	b.ResetTimer()
+
+	b.RunParallel(func(p *testing.PB) {
+		i := 0
+		for p.Next() {
+			rateLimiter.Allow(fmt.Sprintf("key%d", i))
+			i++
+			i = i % 10
+		}
+	})
+}
